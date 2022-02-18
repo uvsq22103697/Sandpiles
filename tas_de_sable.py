667928c8c058501b5d12a57e5423921c7382f060
uvsq22103697 <lea.lapalut@ens.uvsq.fr>
2022-02-18 16:03:10 +0100

    seance 1

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,3 +1,4 @@
+# informations liées au groupe
 #########################################
 # groupe BI 4
 # Flora GHULAM
@@ -5,3 +6,34 @@
 # Tatiana
 # https://github.com/uvsq22103697/Sandpiles
 #########################################
+
+####Pour Tatiana : 
+# chaque '#' correspond à une tâche à faire, à des séquences pour organiser notre programme.(c'est comme le plan d'un cours)
+# c'est Léa la responsble du github et pour la relecture on a pas encore décidé
+# oublie pas de rajouter ton nom dans le créneau tout en haut des informations liées au groupe ;)
+# tu as besoin de savoir comment faire pour partager ton avancé sur le programme, tout est dans le pdf du pro sur moodle, on pourra t'expliquer si tu veux
+# on a créé un espace question (##FAQ ^^) pour communiquer directement sur le programme, si tu as des remarques sur une ligne de code met un '#' et dis nous tout
+# supprime l'espace '####Pour Tatiana' quand tu l'as lu qu'on oublie pas !
+
+## FAQ ^^:
+# 
+
+
+# import des librairies
+import tkinter as Tk
+
+
+
+# définition des constantes (écrites en majuscule)
+
+
+# définition des variables globales
+
+
+# définition des fonctions (chaque fonction devra contenir une docstring)
+
+
+# programme principal contenant la définition des widgets et des événements qui leur sont liés et l’appel à la boucle de gestion des événements
+
+
+
