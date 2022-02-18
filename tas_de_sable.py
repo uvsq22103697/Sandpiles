8de92d884cfba080060c9155030f44396b0062d5
uvsq22103697 <lea.lapalut@ens.uvsq.fr>
2022-02-18 16:40:35 +0100

    seace 1

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -12,11 +12,24 @@
 # c'est Léa la responsble du github et pour la relecture on a pas encore décidé
 # oublie pas de rajouter ton nom dans le créneau tout en haut des informations liées au groupe ;)
 # tu as besoin de savoir comment faire pour partager ton avancé sur le programme, tout est dans le pdf du pro sur moodle, on pourra t'expliquer si tu veux
-# on a créé un espace question (##FAQ ^^) pour communiquer directement sur le programme, si tu as des remarques sur une ligne de code met un '#' et dis nous tout
+# on a créé un espace question (##FAQ ^^) pour communiquer directement sur le programme, si tu as des remarques sur une ligne de code met un '#' et dis nous tout en précisant que c'est toi qui écrit
+# fait toujours un pull quand tu retourne sur le project pour mettre à jour le programme !!! sinon github nous fais la tete et sa fait tout bugger :(
+# si t'es perdue on a aussi repris les consignes du pdf du project dans '#### etapes à effectuer prochainement', efface celle qui tu as déjà faite c'est plus simple !
 # supprime l'espace '####Pour Tatiana' quand tu l'as lu qu'on oublie pas !
 
-## FAQ ^^:
+
+#### Etapes à effectuer prochainement :
+# 1. créer une interface graphique qui permette d’afficher un canevas et un bouton qui créera la configuration aléatoire;
+# 2. la configuration qui s’affiche dans le canevas est appelée la configuration courante et peut être stockée dans une liste à deux dimensions ayant le statut de variable globale; de même pour les éléments graphiques du canevas qui sont associés à l’affichage de la grille;
+# 3. programmer une fonction qui initialise la configuration courante et l’affichage de la grille avec une configuration vide de grains de sable;
+# 4. programmer une fonction qui met à jour l’affichage de la grille à partir de la configuration courante; en particulier il faut associer une couleur à chaque valeur d’une configuration (à choisir);
+# 5. programmer le calcul d’une configuration aléatoire (chaque case reçoit entre 0 et 3 grains de sable de manière équiprobable) : quand on clique sur le bouton, la configuration courante doit être modifiée ainsi que l’affichage de la grille;
+
+
+#### FAQ ^^:
 # 
+
+
 
 
 # import des librairies
